b3b09e84209078ca196ccf9466b5b196795c730a
Wenega Wama <narcissewatherloy@gmail.com>
2022-08-10 15:41:14 -0300

    Added update-account-feature

--- src/index.js
+++ src/index.js
@@ -112,6 +112,23 @@ app.get("/statement/date", verifyExistsAccountCPF, (request, response) => {
     return response.json(statement)   
 })
 
+//Update account
+
+app.put("/account", verifyExistsAccountCPF, (request, response) => {
+    const { name } = request.body
+    const { customer } = request
+
+    customer.name = name
+
+    return response.status(201).send()
+})
+
+//Get information of account
+
+app.get("/account", verifyExistsAccountCPF, (request, response) => {
+    const { customer } = request
+    return response.json(customer)
+} )
 
 app.listen(8080)
 
